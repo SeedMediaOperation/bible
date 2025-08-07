68b67ba4e2a1add29f7afa72df5fe9f33a3a8fd7
Tenghuy <lytenghuy8@gmail.com>
2025-08-07 12:15:57 +0700

    updated

--- app/[locale]/[book]/page.tsx
+++ app/[locale]/[book]/page.tsx
@@ -4,10 +4,10 @@ import ReadingPage from '@/app/[locale]/components/ReadingPage';
 import Image from 'next/image';
 import Navbar from "@/app/[locale]/components/Navbar";
 import Footer from "@/app/[locale]/components/Footer";
-import MediaCard from "@/app/[locale]/components/MediaCard";
+// import MediaCard from "@/app/[locale]/components/MediaCard";
 import {apiGet} from "@/utils/apiHelpers";
 
-import type {Book, Chapters, Media, Version} from "@/types/book";
+import type {Book, Chapters, Version} from "@/types/book";
 import { getLocale } from 'next-intl/server';
 
 export async function generateStaticParams() {
@@ -56,8 +56,8 @@ const Book = async ({ params }: Props) => {
     const chapters = allChapters.filter(chapter => bookIds.includes(String(chapter.bookId)));
     
     // Get Media
-    const resMedia = await apiGet<{ data:Media[] }>(`/api/medias`);
-    const medias = resMedia.data;
+    // const resMedia = await apiGet<{ data:Media[] }>(`/api/medias`);
+    // const medias = resMedia.data;
 
   return (
     <div>
@@ -95,8 +95,7 @@ const Book = async ({ params }: Props) => {
       </div>
 
       {/* Swiper Section */}
-      <div className="relative max-w-[1200px] mx-auto px-4  translate-y-[-7%]">
-          {/* Navigation Buttons */}
+      {/* <div className="relative max-w-[1200px] mx-auto px-4  translate-y-[-7%]">
         <div className="swiper-button-prev-custom absolute top-[50%] -left-1 xl:-left-12 -translate-y-1/2  p-2 rounded-full z-10 cursor-pointer text-gray-400">
           <svg  xmlns="http://www.w3.org/2000/svg"  width="60"  height="60"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-caret-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13.883 5.007l.058 -.005h.118l.058 .005l.06 .009l.052 .01l.108 .032l.067 .027l.132 .07l.09 .065l.081 .073l.083 .094l.054 .077l.054 .096l.017 .036l.027 .067l.032 .108l.01 .053l.01 .06l.004 .057l.002 .059v12c0 .852 -.986 1.297 -1.623 .783l-.084 -.076l-6 -6a1 1 0 0 1 -.083 -1.32l.083 -.094l6 -6l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01z" /></svg>
         </div>
@@ -104,8 +103,8 @@ const Book = async ({ params }: Props) => {
           <svg  xmlns="http://www.w3.org/2000/svg"  width="60"  height="60"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-caret-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6c0 -.852 .986 -1.297 1.623 -.783l.084 .076l6 6a1 1 0 0 1 .083 1.32l-.083 .094l-6 6l-.094 .083l-.077 .054l-.096 .054l-.036 .017l-.067 .027l-.108 .032l-.053 .01l-.06 .01l-.057 .004l-.059 .002l-.059 -.002l-.058 -.005l-.06 -.009l-.052 -.01l-.108 -.032l-.067 -.027l-.132 -.07l-.09 -.065l-.081 -.073l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057l-.002 -12.059z" /></svg>
         </div>
         
-        <MediaCard medias={medias}/>
-      </div>
+         <MediaCard medias={medias}/>
+      </div> */}
       <Footer />
     </div>
   );
